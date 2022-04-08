fd8c5ae708849c92096f94e00254f0713503e09d
basilomp <k0m4w1@gmail.com>
2022-04-08 15:11:53 +0300

    First lesson

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,39 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "FirstServlet", urlPatterns = "/first_servlet")
+public class FirstServlet implements Servlet {
+    private transient ServletConfig config;
+    private static Logger logger = LoggerFactory.getLogger(FirstServlet.class);
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        logger.info("New request");
+        servletResponse.getWriter().println("<h1>My first servlet</h1>");
+        logger.info("Request close");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "FirstServlet";
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("Destroyed");
+    }
+}
